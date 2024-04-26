15bfc7ca0ad0503862dd931d8ade95340f5eaa06
flcampanelli <flcampanelli@gmail.com>
2024-04-25 21:27:10 -0300

    feat(user): implement user entity and resource

---
+++ src/main/java/br/com/app/ecommerce/resources/UserResource.java
@@ -0,0 +1,17 @@
+package br.com.app.ecommerce.resources;
+
+import br.com.app.ecommerce.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Endrick", "endrick@gmail.com", "999511999", "12345678");
+        return ResponseEntity.ok().body(user);
+    }
+}
