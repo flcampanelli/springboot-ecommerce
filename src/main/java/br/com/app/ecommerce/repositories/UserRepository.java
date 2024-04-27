f351d75f725b78f7b86b2ca25e5c5eaab677fb16
flcampanelli <flcampanelli@gmail.com>
2024-04-27 10:30:53 -0300

    feat(UserService): implement service layer and component registration

--- src/main/java/br/com/app/ecommerce/repositories/UserRepository.java
+++ src/main/java/br/com/app/ecommerce/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package br.com.app.ecommerce.repositories;
 import br.com.app.ecommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// @Repository - not necessary because this interface extends JpaRepository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
