4ac62b649251fe3c91b9c9ebc737cd8c757c1619
flcampanelli <flcampanelli@gmail.com>
2024-04-27 10:56:36 -0300

    feat(order): implement order entity

---
+++ src/main/java/br/com/app/ecommerce/entities/Order.java
@@ -0,0 +1,67 @@
+package br.com.app.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "orders")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private User customer;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, User customer) {
+        this.id = id;
+        this.moment = moment;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
